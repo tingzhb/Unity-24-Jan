ab6b4ced2aae4163a09990ff96acbf64f77629e7
Benjamin Ting <benjamin.ting@student.forsbergsskola.se>
2022-01-24 11:59:35 +0100

    Added Player Horizontal Movement and Jumping

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	[SerializeField] private float moveSpeed;
+	[SerializeField] private float jumpForce;
+	[SerializeField] private new Rigidbody rigidbody;
+
+	private void Start() {
+		rigidbody = GetComponent<Rigidbody>();
+	}
+
+	private void Update() {
+		var moveInput = Input.GetAxis("Horizontal");
+		rigidbody.velocity = new Vector3(moveInput * moveSpeed, rigidbody.velocity.y, 0);
+
+		var jumpInput = Input.GetKeyDown(KeyCode.Space);
+		if (jumpInput && rigidbody.velocity.y == 0) {
+			rigidbody.AddForce(Vector3.up * jumpForce);
+		}
+	}
+}
